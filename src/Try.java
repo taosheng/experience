6ba2e66074dca3c48fe739d574ee197a1d709728
Tao-Sheng <taosheng.chen@gmail.com>
2012-02-28 15:02:22 +0800

    add Try

---
+++ src/Try.java
@@ -0,0 +1,14 @@
+public class Try {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		println("try on something");
+	}
+
+	public static void println(Object msg){
+		System.out.println(msg);
+	}
+}
