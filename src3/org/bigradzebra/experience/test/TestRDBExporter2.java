d95f4efbf05e47cad4c18f5add8b47e07482155c
tao-sheng_chen <taosheng.chen@gmail.com>
2012-03-01 15:13:10 +0800

    initial RDBExporter, though still empty

---
+++ src3/org/bigradzebra/experience/test/TestRDBExporter2.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package org.bigradzebra.experience.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author tao-sheng_chen
+ *
+ */
+public class TestRDBExporter2 {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
